0d77c7962c64da12501ee1f6c3c2584e600b9e05
Daniel J Castillo <87207450+dcastillo793@users.noreply.github.com>
2021-07-27 18:47:43 -0700

    Add files via upload
    Not completed well I'm sure but this was really frustrating.

---
+++ helloworld.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+echo "helloworld"
+NAI=$(ifconfig)
+echo "$NAI"
+$NAI > output.txt
+
+# Hello World Script
+# Author: Dan Castillo
+# Last Rev: 20210727
+# Purpose: Learning how to contribute to GitHub with the Ubuntu command line
+# No Variables: one NAI variable for the ifconfig command
+# No Functions
+# Main
+# End
