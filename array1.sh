dea1ef39887b96a182d60009c98f57a91abd4175
daniel <d.castillo.793@gmail.com>
2021-07-30 00:08:55 +0000

    ops challenge 04 fail

---
+++ array1.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Script: Create an array
+# Author: Dan Castillo
+# Date of Latest Revision: 29 July 2021
+# Purpose: Learning!
+
+# make each directory
+#mkdir dir1 dir2 dir3 dir4
+
+# add directories to an array
+directories=("/Test2/dir1" "/Test2/dir2" "/Test2/dir3" "/Test2/dir4")
+
+# adds new text file to each directory
+for eachdir in ${directories[@]}
+do
+    $eachdir touch "new.txt." 
+done
