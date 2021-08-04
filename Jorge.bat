04914b8bc99b1c8bb51e079250286f70f1fa7438
daniel <d.castillo.793@gmail.com>
2021-08-04 23:44:07 +0000

    batch file to copy folders

---
+++ Jorge.bat
@@ -0,0 +1,10 @@
+
+# batch file automation
+# Dan Castillo
+# 4 August 2021
+# Learning
+
+# this copies Jorge's files from his dsktop to his newly mounted E drive
+robocopy C:\Users\j.thompson\desktop E:\Users\j.thompson
+
+# The automation of this task has to be completed with Create Task in Task Scheduler.
